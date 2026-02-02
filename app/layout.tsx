b4cc7c6829bd00c667d1d8f5e9cafd5a5474fb19
Vineet10042 <puneetsingh7800@gmail.com>
2026-02-02 10:58:17 +0530

    chapter 4 & 5 added

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import {ClerkProvider} from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/theme-provider";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,7 +31,16 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
-        {children}
+        <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            <Toaster/>
+            {children}
+          </ThemeProvider>
+        
       </body>
     </html>
     </ClerkProvider>
---
+++ modules/profile/actions/index.ts
@@ -0,0 +1,44 @@
+"use server"
+
+import { db } from '@/lib/db'
+import { currentUser } from '@clerk/nextjs/server'
+import { getAvailableUsernameSuggestions } from "../utils/index";
+
+
+export const checkProfileUsernameAvailability = async (username:string) => {
+    if (!username) return { available: false, suggestions: [] };
+
+    const user = await db.user.findUnique({
+        where: {
+            username: username,
+        },
+    });
+    if(!user) {
+        return { available: true };
+    }
+
+    const suggestions = await getAvailableUsernameSuggestions(username, 3, 10)
+
+    return { available: false, suggestions };
+};
+
+
+export const claimUsername = async (username: string) => {
+  const loggedInUser = await currentUser();
+
+  if (!loggedInUser) return { success: false, error: "No authenticated user found" };
+  
+  const user = await db.user.update({
+    where:{
+        clerkId: loggedInUser.id
+    },
+    data: {
+        username: username
+    }
+  });
+
+  if (!user) return { success: false, error: "No authenticated user found" };
+
+  return { success: true };
+
+}
